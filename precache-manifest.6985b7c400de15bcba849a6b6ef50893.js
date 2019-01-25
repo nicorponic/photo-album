a386f0f99767ab6d09adb0638d2c3de40e7caaec
nicorponic <n1co_frago@hotmail.com>
2019-01-25 17:45:49 +0100

    Updates

---
+++ precache-manifest.6985b7c400de15bcba849a6b6ef50893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "624f7cff49548d33371c",
+    "url": "/photo-album/static/css/main.e7889522.chunk.css"
+  },
+  {
+    "revision": "624f7cff49548d33371c",
+    "url": "/photo-album/static/js/main.624f7cff.chunk.js"
+  },
+  {
+    "revision": "80cc6f04f8f7117cd03f",
+    "url": "/photo-album/static/js/1.80cc6f04.chunk.js"
+  },
+  {
+    "revision": "a127a3d9b22ddb78f43c",
+    "url": "/photo-album/static/js/runtime~main.a127a3d9.js"
+  },
+  {
+    "revision": "b94e1e80f6de36c9f648a5b26d4ae04a",
+    "url": "/photo-album/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/doglist/precache-manifest.e0f78e471059ab921b892563b2197f45.js"
+  "/photo-album/precache-manifest.6985b7c400de15bcba849a6b6ef50893.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/doglist/index.html", {
+workbox.routing.registerNavigationRoute("/photo-album/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
